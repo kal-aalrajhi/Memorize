6f50e0a24b6cd55e73f79c812c333af94eba9d0d
Kal <kal.aalrajhi@gmail.com>
2023-06-28 19:40:20 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Dr Cpt Blackbeard on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
